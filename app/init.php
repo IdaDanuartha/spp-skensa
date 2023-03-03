d9ecf1e211277a5bec7576cbf1244e76da5f96a8
IdaDanuartha <danuart21@gmail.com>
2023-03-03 22:45:07 +0800

    little update

--- app/init.php
+++ app/init.php
@@ -8,4 +8,4 @@ require_once "core/Database.php";
 require_once "core/Flasher.php";
 require_once "core/Middleware.php";
 require_once "core/Model.php";
-require_once "helpers/functions.php";
+require_once "helpers/functions.php"; 
